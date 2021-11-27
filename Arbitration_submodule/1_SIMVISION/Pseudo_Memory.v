5958c7c5976dcae33c32c3e40b8f0a32d499cc45
Odysseas Papadakis <odypapadakis@gmail.com>
2021-11-27 20:52:53 +0200

    seperate files is not worth it

--- Arbitration_submodule/1_SIMVISION/Pseudo_Memory.v
+++ Arbitration_submodule/1_SIMVISION/Pseudo_Memory.v
@@ -1,77 +1,85 @@
 `timescale 1ns / 1ns   // <time_unit> / <time_precision>
 
 
-module Pseudo_Memory;
+module Pseudo_Memory(
+
+input clk;
+input reset;
+
+
+);
 
 
 
-// Pseudo Data memory States
-reg [1:0] Pseudo_D_Memory_Current_State,Pseudo_D_Memory_Next_State ;
 
-localparam	Pseudo_D_Memory_State_Idle 				= 2'b00 ;
-localparam	Pseudo_D_Memory_State_Read_HIGH 		= 2'b01 ;
-//localparam	Pseudo_D_Memory_State_Wait_MEM_LOW 	= 2'b11 ;
+// Pseudo Instruction memory states
+
+reg [1:0] Pseudo_I_Memory_Current_State,Pseudo_I_Memory_Next_State ;
+
+localparam	Pseudo_I_Memory_State_Idle 				= 2'b00 ;
+localparam	Pseudo_I_Memory_State_Read_HIGH 		= 2'b01 ;
+//localparam	Pseudo_I_Memory_State_Wait_MEM_LOW 	= 2'b11 ;
 
 
 
-//	Pseudo Data Memory Sequential always Block
+//	Pseudo Instruction Memory Sequential always Block
 
 always@(posedge clk or posedge reset) 
 	if(reset)
-		#1 Pseudo_D_Memory_Current_State <= Pseudo_D_Memory_State_Idle ; // On reset, go to the Idle state
+		#1 Pseudo_I_Memory_Current_State <= Pseudo_I_Memory_State_Idle ; // On reset, go to the Idle state
 	else
-		#1 Pseudo_D_Memory_Current_State <= Pseudo_D_Memory_Next_State ; // If not resetting, start sequencing the states
+		#1 Pseudo_I_Memory_Current_State <= Pseudo_I_Memory_Next_State ; // If not resetting, start sequencing the states
 
 
 
 
-//	Pseudo Data Memory combinational always Block
+//	Pseudo Instruction Memory combinational always Block
 always@(*)
 begin
-	case(Pseudo_D_Memory_Current_State)
+	case(Pseudo_I_Memory_Current_State)
 //------------------------------------------------------------------------------------------
-	Pseudo_D_Memory_State_Idle: 	
+	Pseudo_I_Memory_State_Idle: 	
 		begin
-			
 			// Show that the data is not valid 
-			#(5*m) tb_Bus_DataMem_Ready = 0'b0;
+			#5 tb_Bus_InstMem_Ready = 0'b0;
 
-			// Idle 
-			#(5*m) tb_Bus_DataMem_In = 32'dx;
+			//Idle 
+			#5 tb_Bus_InstMem_In = 32'dx;
 
 			// When the read signal goes high
-			if( (tb_Bus_DataMem_Read == 1'b1) )
-				#(5*m) Pseudo_D_Memory_Next_State = Pseudo_D_Memory_State_Read_HIGH;
+			if( (tb_Bus_InstMem_Read == 1'b1) )
+				#5 Pseudo_I_Memory_Next_State = Pseudo_I_Memory_State_Read_HIGH;
 			else
-				Pseudo_D_Memory_Next_State = Pseudo_D_Memory_State_Idle;
+				Pseudo_I_Memory_Next_State = Pseudo_I_Memory_State_Idle;
 
 		end	
 //------------------------------------------------------------------------------------------
-	Pseudo_D_Memory_State_Read_HIGH:
+	Pseudo_I_Memory_State_Read_HIGH:
 		begin
-			// Add 6 to the Address and return it as data
-			#(5*m) tb_Bus_DataMem_In = tb_Bus_DataMem_Address + 32'd6;
+			// Add 4 to the Address and return it as data
+			#20 tb_Bus_InstMem_In = tb_Bus_InstMem_Address + 32'd4;
 
 			// Raise the ready signal, to inform that the data being served is valid
-			#(15*m) tb_Bus_DataMem_Ready = 1'b1;
+			#35 tb_Bus_InstMem_Ready = 1'b1;
 
 			// When the read signal goes low move on to Idle
-			if(tb_Bus_DataMem_Read == 1'b0)
+			if(tb_Bus_InstMem_Read == 1'b0)
 				begin
-					#5 Pseudo_D_Memory_Next_State = Pseudo_D_Memory_State_Idle  ;
+					#5 Pseudo_I_Memory_Next_State = Pseudo_I_Memory_State_Idle  ;
 				end
 			else
-				Pseudo_D_Memory_Next_State = Pseudo_D_Memory_State_Read_HIGH;	
+				Pseudo_I_Memory_Next_State = Pseudo_I_Memory_State_Read_HIGH;	
 			end
 //------------------------------------------------------------------------------------------
 	default:
 		begin
-			Pseudo_D_Memory_Next_State = Pseudo_D_Memory_State_Idle ;
+			Pseudo_I_Memory_Next_State = Pseudo_I_Memory_State_Idle ;
 		end
 
 	endcase	
 
 end
+
 
 
 endmodule
